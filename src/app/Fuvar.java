cea3649bde326872968c7dc1952236e9dfa57732
Csillag Gergő Zsolt (SZF_N_2025_2027) <csillag.gergo.zsolt@diak.szamalk-szalezi.hu>
2026-05-08 11:14:59 +0200

    masodik commit

--- src/app/Fuvar.java
+++ src/app/Fuvar.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package app;
 
-/**
- *
- * @author CsillagGergőZsolt(SZ
- */
 public class Fuvar {
-    
+
+    private String rendszam;
+    private int ido;
+    private int osszeg;
+    private String fizetesiMod;
+
+    public Fuvar(String rendszam, int ido, int osszeg, String fizetesiMod) {
+        this.rendszam = rendszam;
+        this.ido = ido;
+        this.osszeg = osszeg;
+        this.fizetesiMod = fizetesiMod;
+    }
+
+    public String getRendszam() {
+        return rendszam;
+    }
+
+    public int getIdo() {
+        return ido;
+    }
+
+    public int getOsszeg() {
+        return osszeg;
+    }
+
+    public String getFizetesiMod() {
+        return fizetesiMod;
+    }
 }
--- src/app/Fuvarok.java
+++ src/app/Fuvarok.java
@@ -1,9 +1,37 @@
 package app;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fuvarok {
 
-    public static void main(String[] args) {
-        
+    private static List<Fuvar> fuvarok = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        Path path = Path.of("fuvarok.csv");
+
+        List<String> sorok = Files.readAllLines(path);
+
+        System.out.println(sorok.get(0));
+
+        for (String sor : sorok) {
+            String s[] = sor.split(",");
+            String rendszam = s[0];
+            int ido = Integer.parseInt(s[1]);
+            int osszeg = Integer.parseInt(s[2]);
+            String fizetesiMod = s[3];
+            Fuvar fuvar = new Fuvar(rendszam, ido, osszeg, fizetesiMod);
+            fuvarok.add(fuvar);
+
+        }
+        int osszeg = 0;
+        for (int i = 0; i < fuvarok.size(); i++) {
+            osszeg += fuvarok.get(i).getOsszeg();
+        }
+        System.out.println("osszeg: ");
     }
 
 }
